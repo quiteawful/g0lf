bfd7d10fc73f103eb4057afcac6a3aedb42dcf87
lucron <patric.halama@g-fit.de>
2014-11-03 11:09:56 +0100

    more vector stuff

--- vector.go
+++ vector.go
@@ -1,25 +1,41 @@
 package main
 
+import "math"
+
 type Vec [2]float64
 
 const precision = 0.0000001
 
+// Add adds vector a to v
 func (v *Vec) Add(a *Vec) *Vec {
 	v[0] = v[0] + a[0]
 	v[1] = v[1] + a[1]
 	return v
 }
 
-func (v *Vec) Del(a *Vec) *Vec {
+// Sub substracts vector a from v
+func (v *Vec) Sub(a *Vec) *Vec {
 	v[0] = v[0] - a[0]
 	v[1] = v[1] - a[1]
 	return v
 }
 
+// Equals compares two vectors and returns true if the difference
+// is smaller than the defined precision
 func (v *Vec) Equals(a *Vec) bool {
-	diff := v.Del(a)
+	diff := v.Sub(a)
 	if diff[0] < precision && diff[1] < precision {
 		return true
 	}
 	return false
+}
+
+// Dotp calculates the dot product of two vectors (Skalarprodukt)
+func Dotp(a, b *Vec) float64 {
+	return a[0]*b[0] + a[1]*b[1]
+}
+
+// length returns the length of a vector
+func (v *Vec) length() float64 {
+	return math.Sqrt(Dotp(v, v))
 }
